551dbc03e79adfd5f66f51078251baa4e841ead9
Tarunika4 <93110264+Tarunika4@users.noreply.github.com>
2022-03-18 09:02:08 +0000

    done

--- p2original.c
+++ p2original.c
@@ -2,33 +2,38 @@
 int input_side()
 {
   int a;
-  printf("enter the a value:");
+  printf("enter a side length: \n");
   scanf("%d",&a);
   return a;
 }
-int check_scalene(int a,int b,int c)
+int check_scalene(int a, int b,int c)
 {
   int isscalene;
-  if(a!=b && b!=c)
-  return 0;
-  else
-  return 1;
-  
+  if (a!=b && a!=c && b!=c)
+  {
+    isscalene=0;
+  }
+  return isscalene;
 }
 void output(int a,int b,int c,int isscalene)
 {
   if(isscalene==0)
-  printf("SCALENE");
+  {
+    printf("The triange is scalene for lengths %d,%d and %d",a,b,c);
+  }
   else
- printf("NOT SCALENE"); 
+  {
+    printf("The triange is not scalene");
+  }
 }
+
 int main()
 {
-  int a,b,c,d;
+  int a,b,c,isscalene;
   a=input_side();
   b=input_side();
-  c=a=input_side();
-  d=check_scalene(a,b,c);
-  output(a,b,c,d);
+  c=input_side();
+  isscalene=check_scalene(a,b,c);
+  output(a,b,c,isscalene);
   return 0;
 }
